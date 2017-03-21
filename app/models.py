70b268ce56b8bdcafbe58f5cb432ace24cf647cd
Chris Puppe <chrispuppe@gmail.com>
2017-03-21 15:59:41 -0700

    Users added to models.py.

--- app/models.py
+++ app/models.py
@@ -2,7 +2,13 @@ from app import db
 from sqlalchemy.dialects.mysql import BOOLEAN
 from sqlalchemy import ForeignKey
 import datetime
+from flask_login import UserMixin
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
 
 class Employee(db.Model):
     __tablename__ = 'employee'
